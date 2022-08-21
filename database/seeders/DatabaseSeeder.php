0a7fea66b89add2b5d6b61d7d0a4b861fd38762a
Tariq Ayman <tariq.ayman94@gmail.com>
2022-08-21 02:51:36 +0200

    add seeder for users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'email' => 'admin@admin.com',
+            'user_type' => 'admin',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->count(100)->create([
+            'user_type' => 'admin',
+        ]);
+
+        User::factory()->count(1000)->create();
+
+//        Task::factory()->count(100)->create();
     }
 }
