c4c9bd6c007acc1fa7559de76d69010cb916f109
Tariq Ayman <tariq.ayman94@gmail.com>
2022-08-21 02:50:37 +0200

    create tasks migration file

---
+++ database/migrations/2022_08_20_110232_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('assigned_to_id');
+            $table->foreign('assigned_to_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('assigned_by_id');
+            $table->foreign('assigned_by_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
